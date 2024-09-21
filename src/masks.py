d456137f02815649e49d36ec98691f06236dc6b7
Alena <alittlekindness94@gmail.com>
2024-09-21 16:21:39 +0500

    update 2

--- src/masks.py
+++ src/masks.py
@@ -1,16 +1,14 @@
-
-
 # entering_the_account_number = input("Enter your entering account number: ")
 # entering_the_card_number = input("Enter the card number: ")
 
 
 def get_mask_card_number(entering_the_card_number: str) -> str:
-     """Функция, возвращающая маску карты"""
-     return f"{entering_the_card_number[:4]} {entering_the_card_number[4:6]}** **** {entering_the_card_number[12:]}"
+    """Функция возвращающая маску карты"""
+    return f"{entering_the_card_number[:4]} {entering_the_card_number[4:6]}** **** {entering_the_card_number[12:]}"
 
 
 def get_mask_account(entering_the_account_number: str) -> str:
-    """Функция, возвращающая маску счета"""
+    """Функция возвращающая маску счета"""
     return f"**{entering_the_account_number[16:]}"
 
 
--- src/widget.py
+++ src/widget.py
@@ -1,16 +1,15 @@
-from src.masks import  get_mask_account, get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(number: str) -> str:
-    """Функция, маскирующая счет/карту"""
+    """Функция маскирующая счет/карту"""
     if "Счет" in number:
-        return f'{number[:-20]}{get_mask_account(number[-20:])}'
+        return f"{number[:-20]}{get_mask_account(number[-20:])}"
     else:
-        return f'{number[:-16]}{get_mask_card_number(number[-16:])}'
+        return f"{number[:-16]}{get_mask_card_number(number[-16:])}"
 
 
 def get_new_data(old_data: str) -> str:
     """Функция принимает строку и выводит дату в формате dd.mm.yyyy"""
     data_slize = old_data[0:10].split("-")
     return ".".join(data_slize[::-1])
-
