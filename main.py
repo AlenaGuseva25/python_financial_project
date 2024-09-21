f9d69970af15a45bf6e540ab430664cb679941a7
Alena <alittlekindness94@gmail.com>
2024-09-21 16:10:35 +0500

    update

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from src.masks import get_mask_account
+# from src.masks import get_mask_account
+#
+# from src.masks import get_mask_card_number
+#
+# print(get_mask_account("73654108430135874305"))
+# print(get_mask_card_number("7000792289606361"))
 
-from src.masks import get_mask_card_number
+from src.widget import mask_account_card
 
-print(get_mask_account("73654108430135874305"))
-print(get_mask_card_number("7000792289606361"))
+from src.widget import get_new_data
+
+print(mask_account_card("Счет 64686473678894779589"))
+print(get_new_data("2024-04-11T02:26:18.671407"))
---
+++ src/widget.py
@@ -0,0 +1,16 @@
+from src.masks import  get_mask_account, get_mask_card_number
+
+
+def mask_account_card(number: str) -> str:
+    """Функция, маскирующая счет/карту"""
+    if "Счет" in number:
+        return f'{number[:-20]}{get_mask_account(number[-20:])}'
+    else:
+        return f'{number[:-16]}{get_mask_card_number(number[-16:])}'
+
+
+def get_new_data(old_data: str) -> str:
+    """Функция принимает строку и выводит дату в формате dd.mm.yyyy"""
+    data_slize = old_data[0:10].split("-")
+    return ".".join(data_slize[::-1])
+
