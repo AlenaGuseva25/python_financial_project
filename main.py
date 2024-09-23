7eab9ad931c77661c76bc042c63735f488744b8c
Alena <kindness25a@gmail.com>
2024-09-23 11:44:36 +0500

    init

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ from src.widget import mask_account_card
 from src.widget import get_new_data
 
 print(mask_account_card("Счет 64686473678894779589"))
-print(get_new_data("2024-04-11T02:26:18.671407"))
+print(get_new_data("2024-04-1   1T02:26:18.671407"))
