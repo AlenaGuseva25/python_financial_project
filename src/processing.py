4450a0d6866749fb3001bfe08b9297a93646a8bc
Alena <alittlekindness94@gmail.com>
2024-09-25 15:13:36 +0500

    update function

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,6 @@
-def filter_by_state():
+def filter_by_state(data: list[dict[str, str]], state: str = "EXECUTED") -> list[dict[str, str]]:
     """Функция возвращает список словарей по ключу state с указанным значением"""
-    pass
+    return [d for d in data if d.get("state") == state]
 
 
 def sort_by_date():
