a0a112765e4e9146d2edf58af8496bd93ff5d74a
Alena <alittlekindness94@gmail.com>
2024-09-29 19:12:42 +0500

    test processing

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(data: list[dict[str, str]], state: str = "EXECUTED") -> list[dict[str, str]]:
+def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция возвращает список словарей по ключу state с указанным значением"""
     return [d for d in data if d.get("state") == state]
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -16,3 +16,61 @@ def date() -> str:
 @pytest.fixture
 def total_date() -> str:
     return "11.03.2024"
+
+@pytest.fixture
+def data() -> list[dict]:
+    return [{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}]
+
+@pytest.fixture
+def state() -> str:
+    return "EXECUTED"
+
+@pytest.fixture
+def state_1() -> str:
+    return "CANCELED"
+
+@pytest.fixture
+def tota() -> list[dict]:
+    return [{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}]
+
+@pytest.fixture
+def tota_1() -> list[dict]:
+    return [{"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}]
+
+@pytest.fixture
+def conclusion() -> list[dict]:
+    return [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
+]
+
+@pytest.fixture
+def date_list() -> list[dict]:
+    return [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
+]
+
+@pytest.fixture
+def direction() -> bool:
+    return True
+
+@pytest.fixture
+def direction_2() -> bool:
+    return False
+
+@pytest.fixture
+def conclusion_2() -> list[dict]:
+    return [{"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}]
