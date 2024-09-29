dde52c82c07862f45c91f653a7544eec19103a0c
AlenaGuseva25 <kindness25a@gmail.com>
2024-09-29 19:58:53 +0500

    Update processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,9 @@
-def filter_by_state(data: list[dict[str, str]], state: str = "EXECUTED") -> list[dict[str, str]]:
+def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция возвращает список словарей по ключу state с указанным значением"""
     return [d for d in data if d.get("state") == state]
 
 
-def sort_by_date(date_list: list, direction: bool = True) -> list:
+def sort_by_date(date_list: list[dict], direction: bool = True) -> list[dict]:
     """Функция сортировки списка словарей по дате"""
     sorted_list = sorted(date_list, key=lambda x: x.get("date"), reverse=direction)
     return sorted_list
