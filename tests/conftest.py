000768713419fc46d864844e5df17ec071c17a21
Alena <alittlekindness94@gmail.com>
2024-09-29 18:18:17 +0500

    test widget

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,11 @@ def dat() -> str:
 @pytest.fixture
 def expec() -> str:
     return "**8900"
+
+@pytest.fixture
+def date() -> str:
+    return "2024-03-11T02:26:18.671407"
+
+@pytest.fixture
+def total_date() -> str:
+    return "11.03.2024"
