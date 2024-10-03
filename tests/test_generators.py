0ffaf6d5b0c215ae5d51a242831ec2bb8ddb4d22
Alena <alittlekindness94@gmail.com>
2024-10-03 16:46:18 +0500

    tests

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -2,21 +2,21 @@ from src.generators import filter_by_currency, transaction_descriptions, card_nu
 
 
 def test_filter_by_currency(transaction_list, code):
-    output = filter_by_currency(transaction_list)
-    assert next(output) == {
-        "id": 939719570,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {
-            "amount": "9824.07",
-            "currency": {"name": "USD", "code": "USD"},
-        },
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702",
-    }
-    assert next(output) == {
-        "id": 142264268,
+    out = filter_by_currency(transaction_list)
+    assert next(out) == {
+            "id": "939719570",
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {"name": "USD", "code": "USD"},
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
+        }
+    assert next(out) == {
+        "id": "142264268",
         "state": "EXECUTED",
         "date": "2019-04-04T23:20:05.206878",
         "operationAmount": {
@@ -27,8 +27,8 @@ def test_filter_by_currency(transaction_list, code):
         "from": "Счет 19708645243227258542",
         "to": "Счет 75651667383060284188",
     }
-    assert next(output) == {
-        "id": 895315941,
+    assert next(out) == {
+        "id": "895315941",
         "state": "EXECUTED",
         "date": "2018-08-19T04:27:37.904916",
         "operationAmount": {
