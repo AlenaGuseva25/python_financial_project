3259d130bf2cb030fcc661f0729e339872c0f4da
Alena <alittlekindness94@gmail.com>
2024-10-15 16:06:01 +0500

    correction flake8

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,14 @@
 import os
 import json
 import logging
-import requests
+
 # используется для записи сообщений об ошибках в журнал
 logger = logging.getLogger(__name__)
 
 
 def financial_transactions(file_path):
-    """Функция принимает на вход путь до JSON - файла и возвращает список словарей с данными о финансовых транзакциях"""
+    """Функция принимает на вход путь до JSON - файла и возвращает список
+           словарей с данными о финансовых транзакциях"""
     if not os.path.exists(file_path):
         logger.error(f'Файл {file_path} не найден.')
         return []
@@ -18,9 +19,8 @@ def financial_transactions(file_path):
             if isinstance(data, list):
                 return data
             else:
-                logger.error(f'Данные в файле не являются списком.')
+                logger.error(f'Данные в {file_path} не являются списком.')
                 return []
     except json.decoder.JSONDecodeError:
         logger.error('Ошибка декодирования JSON.')
         return []
-
