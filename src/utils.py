42ff1405be07b0160170a65034f4a55809881b6d
Alena <alittlekindness94@gmail.com>
2024-10-17 12:53:21 +0500

    update

--- src/utils.py
+++ src/utils.py
@@ -8,19 +8,20 @@ logger = logging.getLogger(__name__)
 
 def financial_transactions(file_path):
     """Функция принимает на вход путь до JSON - файла и возвращает список
-           словарей с данными о финансовых транзакциях"""
+    словарей с данными о финансовых транзакциях"""
     if not os.path.exists(file_path):
-        logger.error(f'Файл {file_path} не найден.')
+        logger.error(f"Файл {file_path} не найден.")
         return []
     try:
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
-# isinstance для проверки принадлежности объекта к определенному классу или типу данных(новый метод, запомнить)
+            # isinstance для проверки принадлежности объекта к определенному классу или типу данных
+            # (новый метод, запомнить)
             if isinstance(data, list):
                 return data
             else:
-                logger.error(f'Данные в {file_path} не являются списком.')
+                logger.error(f"Данные в {file_path} не являются списком.")
                 return []
     except json.decoder.JSONDecodeError:
-        logger.error('Ошибка декодирования JSON.')
+        logger.error("Ошибка декодирования JSON.")
         return []
---
+++ tests/test_external_api.py
@@ -0,0 +1,38 @@
+from unittest.mock import patch
+
+import pytest
+
+from src.external_api import convert_to_rub
+
+
+@pytest.fixture
+def trans_1():
+    return {"amount": 92688.46, "currency": "USD"}
+
+
+@pytest.fixture
+def trans_2():
+    return {"amount": 92688.46, "currency": "EUR"}
+
+
+@pytest.fixture
+def trans_3():
+    return {"amount": 0, "currency": "USD"}
+
+
+@patch('requests.get')
+def test_currency_conversion(mock_get, trans_1):
+    mock_get.return_value.json.return_value = {"success": True, "query":
+                                               {"from": "USD", "to": "RUB", "amount": "8221.37"}, "info":
+                                               {"timestamp": 1724671757, "rate": 91.475458},
+                                               "date": "2024-08-26", "result": 752053.58}
+    assert convert_to_rub(trans_1) == 752053.58
+
+
+@patch('requests.get')
+def test_currency_conversion_2(mock_get, trans_2):
+    mock_get.return_value.json.return_value = {"success": True, "query":
+                                               {"from": "EUR", "to": "RUB", "amount": "8221.37"}, "info":
+                                               {"timestamp": 1724671757, "rate": 91.475458},
+                                               "date": "2024-08-26", "result": 752053.58}
+    assert convert_to_rub(trans_2) == 752053.58
